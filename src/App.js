dcf1b1a3b02bc4e28a7d77d099c607c53640a18f
ktalmage <ktalmage85@gmail.com>
2020-12-22 17:44:00 -0500

    added pdf page viewer ability

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-
 import './App.css';
+import React from 'react';
+import { PDFReader } from 'reactjs-pdf-reader'
 
 function App() {
   return (
-    <div>fuck</div>
+    <div style={{overflow:'scroll',height:600}}>
+            <PDFReader page={3} url="https://s23.q4cdn.com/714267708/files/doc_downloads/PTP_307111_SAMPLEPARTNERA_K1_2018_FED.PDF" />
+           </div>
   );
 }
 
